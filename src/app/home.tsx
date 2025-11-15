601ae92e32d22fb5bbefb63e98421d5750816ee3
DeeDeevk <khoa01022020@gmail.com>
2025-11-15 10:37:36 +0700

    Tim kiem theo ten

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,14 +1,16 @@
 import { View, Text, FlatList } from "react-native";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { GroceryItem } from "@/types/GroceryItem";
 import { deleteGrocery, getAllGrocery, markBought } from "@/db/db";
 import { useSQLiteContext } from "expo-sqlite";
 import { useFocusEffect } from "expo-router";
 import GroceryItemCard from "@/components/GroceryItemCard";
+import { TextInput } from "react-native-paper";
 
 const Home = () => {
   const db = useSQLiteContext();
   const [groceries, setGroceries] = useState<GroceryItem[]>([]);
+  const [nameSearch, setNameSearch] = useState("");
 
   const handleFetchDb = async () => {
     await getAllGrocery(db).then((res) => setGroceries(res));
@@ -29,10 +31,25 @@ const Home = () => {
     await deleteGrocery(db, id).then(() => handleFetchDb());
   };
 
+  const filteredGroceries = useMemo(() => {
+    return groceries.filter((item) =>
+      item.name.toLowerCase().includes(nameSearch.toLocaleLowerCase())
+    );
+  }, [db, groceries, nameSearch]);
+
   return (
     <View className="flex flex-1">
+      <View className="px-4 gap-4 my-2">
+        <Text className="text-lg">Searching</Text>
+        <TextInput
+          label={"Name"}
+          value={nameSearch}
+          onChangeText={(value) => setNameSearch(value)}
+        />
+      </View>
+
       <FlatList
-        data={groceries}
+        data={filteredGroceries}
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <GroceryItemCard
